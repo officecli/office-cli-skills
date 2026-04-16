0ea8eeba57d9c06db2fb9c53673aa99d043d3334
Void <luyang950@gmail.com>
2026-04-16 06:14:20 +0000

    platform: add session cookie debug tool

---
+++ platform/cmd/debug-session-cookie/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/officecli/officecli/platform/internal/auth"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		panic("usage: debug-session-cookie <secret> <session-id>")
+	}
+	codec := auth.NewSecureCookieCodec(os.Args[1])
+	raw, err := codec.Encode(os.Args[2])
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(raw)
+}
